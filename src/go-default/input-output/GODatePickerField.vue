ebe28c548340fe64f4a6b5eaa728017a72ea6c29
tvillaren <thomas.villaren@generativeobjects.com>
2018-07-19 11:13:23 +0200

    Updating DatePicker to allow edition

--- src/go-default/input-output/GODatePickerField.vue
+++ src/go-default/input-output/GODatePickerField.vue
@@ -25,10 +25,11 @@
                     :value="displayedValue"
                     :label="label"
                     prepend-icon="event"
-                    readonly
+                    @blur="onTextFieldBlur"
+                    @input="onTextFieldChange"
                 ></v-text-field>
                 <v-date-picker 
-                    @input="onInput" 
+                    @input="onDateInput" 
                     no-title 
                     scrollable 
                     first-day-of-week="1" 
@@ -48,7 +49,8 @@ export default {
     },
     data() {
         return {
-            menu: false
+            menu: false,
+            tempDashedDate: null // used when users directly interacts with the Text field
         };
     },
     computed: {
@@ -58,27 +60,77 @@ export default {
         displayedValue() {
             if (!this.value) return null;
 
-            const [year, month, day] = this.dashedDate.split('-');
+            const [year, month, day] = this.getDashedDate(this.value).split(
+                '-'
+            );
             return `${day}/${month}/${year}`;
         },
         // We use the dashed version of the provided Date object (this.value)
         // The object is expected to be a day only
         dashedDate() {
-            if (!this.value) return null;
-            return (
-                this.value.getFullYear() +
-                '-' +
-                ('0' + (this.value.getMonth() + 1)).slice(-2) +
-                '-' +
-                ('0' + this.value.getDate()).slice(-2)
-            );
+            return this.tempDashedDate || this.getDashedDate(this.value);
         }
     },
     methods: {
-        onInput(val) {
+        onDateInput(val) {
             // When emitting the Date object, we make sure it is set to the Greenwhich TZ
             this.$emit('input', new Date(val + 'T00:00:00Z'));
             this.menu = false;
+        },
+        onTextFieldChange(val) {
+            let inputDate = this.getDateFromSlashedDate(val);
+            if (inputDate != null) {
+                this.tempDashedDate = this.getDashedDate(inputDate);
+            }
+        },
+        onTextFieldBlur(evt) {
+            let inputDate = this.getDateFromSlashedDate(evt.target.value);
+            if (inputDate != null) {
+                this.$emit('input', inputDate);
+            }
+            this.tempDashedDate = null;
+        },
+
+        // Returns the JS Date Object corresponding to the parameter if it is
+        // a valid DD/MM/YYYY Date input, null otherwise
+        getDateFromSlashedDate(slashedDate) {
+            let parts = slashedDate.split('/');
+            if (parts.length !== 3) return null;
+
+            let day = parseInt(parts[0]),
+                month = parseInt(parts[1]),
+                year = parseInt(parts[2]);
+            if (isNaN(day) || isNaN(month) || isNaN(year)) {
+                return null;
+            }
+
+            let monthString = month < 10 ? '0' + month : month.toString();
+            let dayString = day < 10 ? '0' + day : day.toString();
+
+            let date = new Date(
+                `${year}-${monthString}-${dayString}T00:00:00Z`
+            );
+
+            if (date instanceof Date && !isNaN(date)) {
+                return date;
+            }
+            return null;
+        },
+        getDashedDate(val) {
+            if (!val || !(val instanceof Date) || isNaN(val)) return null;
+            return (
+                val.getFullYear() +
+                '-' +
+                ('0' + (val.getMonth() + 1)).slice(-2) +
+                '-' +
+                ('0' + val.getDate()).slice(-2)
+            );
+        }
+    },
+    watch: {
+        // Making sure to resync everything is current value is changed from outside
+        value(newVal, oldVal) {
+            this.tempDashedDate = null;
         }
     }
 };
