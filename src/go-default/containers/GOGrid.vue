c2ae4207086264f0c19847445a25878ee6c0b592
tvillaren <thomas.villaren@generativeobjects.com>
2019-02-06 18:05:53 +0100

    Adding pagination & sort handling on GOGrid

--- src/go-default/containers/GOGrid.vue
+++ src/go-default/containers/GOGrid.vue
@@ -4,13 +4,28 @@
             :items="dataToDisplay"
             :headers="headers"
             :loading="loading"
-            hide-actions
+            :total-items="totalItems"
+            :pagination.sync="localPaginationObject"
+            @update:pagination="onPaginationUpdated"
             class="elevation-1"
+            hide-actions
+            disable-initial-sort
         >
             <template slot="items" slot-scope="props">
                 <tr>
                     <slot name="default" :item="props.item"></slot>
                 </tr>
+            </template>
+            <template slot="footer" v-if="pagination && pagination.isPaginationEnabled">
+                <td :colspan="headers.length" class="text-xs-center">
+                    <v-pagination
+                        :length="pagination.totalPage"
+                        v-model="localPaginationObject.page"
+                        v-if="pagination.totalPage > 1 && pagination.currentTotal > 0"
+                        total-visible="6"
+                        @input="onCurrentPageNumberChanged"
+                    ></v-pagination>
+                </td>
             </template>
         </v-data-table>
         <slot name="grid-actions">
@@ -26,14 +41,59 @@ export default {
     props: {
         dataCollection: Array,
         headers: Array,
-        loading: Boolean
+        loading: Boolean,
+        pagination: Object,
+        sort: Object
+    },
+    data() {
+        return {
+            localPaginationObject: {
+                sortBy:
+                    this.sort && this.sort.sortColumn
+                        ? this.sort.sortColumn
+                        : null,
+                descending: this.sort && this.sort.isDescendings
+            }
+        };
     },
     computed: {
         dataToDisplay() {
             return this.dataCollection;
+        },
+        totalItems() {
+            if (this.pagination && this.pagination.isPaginationEnabled) {
+                return this.pagination.currentTotal;
+            }
+            // Returning -1 to tell v-data-table that we handle pagination externally
+            // even when disabled (to capture sort)
+            return -1;
         }
     },
-    components: {}
+    methods: {
+        onCurrentPageNumberChanged(value) {
+            this.$emit("pageChanged", value);
+            this.localPaginationObject.page = value;
+        },
+        onPaginationUpdated(pagination) {
+            console.log(pagination);
+            if (
+                this.pagination &&
+                pagination.page !== this.pagination.currentPageNumber
+            ) {
+                this.$emit("pageChanged", pagination.page);
+            }
+            if (
+                this.sort &&
+                (pagination.sortBy !== this.sort.sortColumn ||
+                    pagination.descending != this.sort.isDescending)
+            ) {
+                this.$emit("sortUpdated", {
+                    sortColumn: pagination.sortBy,
+                    isDescending: pagination.descending
+                });
+            }
+        }
+    }
 };
 </script>
 
