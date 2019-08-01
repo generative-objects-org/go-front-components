a831ed145dceb5726c2f553265a876994fe014c1
tvillaren <thomas.villaren@generativeobjects.com>
2019-08-01 16:29:08 +0200

    Temp version of GOGrid filter-zone

--- src/go-default/containers/GOGrid.vue
+++ src/go-default/containers/GOGrid.vue
@@ -1,37 +1,44 @@
 <template>
-    <v-flex v-if="isVisible">
-        <v-data-table
-            :items="dataToDisplay"
-            :headers="headers"
-            :loading="loading"
-            :total-items="totalItems"
-            :pagination.sync="localPaginationObject"
-            @update:pagination="onPaginationUpdated"
-            class="elevation-1"
-            hide-actions
-            disable-initial-sort
-        >
-            <template slot="items" slot-scope="props">
-                <tr>
-                    <slot name="default" :item="props.item"></slot>
-                </tr>
-            </template>
-            <template slot="footer" v-if="pagination && pagination.isPaginationEnabled">
-                <td :colspan="headers.length" class="text-xs-center">
-                    <v-pagination
-                        :length="pagination.totalPage"
-                        :value="pagination.currentPageNumber"
-                        v-if="pagination.totalPage > 1 && pagination.currentTotal > 0"
-                        total-visible="6"
-                        @input="onCurrentPageNumberChanged"
-                    ></v-pagination>
-                </td>
-            </template>
-        </v-data-table>
-        <slot name="grid-actions">
-            <!-- Slot where form actions will be injected  -->
-        </slot>
-    </v-flex>
+    <v-layout column>
+        <v-flex>
+            <v-layout row>
+                <slot name="filter-zone"></slot>
+            </v-layout>
+        </v-flex>
+        <v-flex v-if="isVisible">
+            <v-data-table
+                :items="dataToDisplay"
+                :headers="headers"
+                :loading="loading"
+                :total-items="totalItems"
+                :pagination.sync="localPaginationObject"
+                @update:pagination="onPaginationUpdated"
+                class="elevation-1"
+                hide-actions
+                disable-initial-sort
+            >
+                <template slot="items" slot-scope="props">
+                    <tr>
+                        <slot name="default" :item="props.item"></slot>
+                    </tr>
+                </template>
+                <template slot="footer" v-if="pagination && pagination.isPaginationEnabled">
+                    <td :colspan="headers.length" class="text-xs-center">
+                        <v-pagination
+                            :length="pagination.totalPage"
+                            :value="pagination.currentPageNumber"
+                            v-if="pagination.totalPage > 1 && pagination.currentTotal > 0"
+                            total-visible="6"
+                            @input="onCurrentPageNumberChanged"
+                        ></v-pagination>
+                    </td>
+                </template>
+            </v-data-table>
+            <slot name="grid-actions">
+                <!-- Slot where form actions will be injected  -->
+            </slot>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script>
